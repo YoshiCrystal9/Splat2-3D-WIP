82a6bca5621abf63a9c2fc56e523a5ac9f102f87
YoshiCrystal9 <89108143+YoshiCrystal9@users.noreply.github.com>
2022-01-16 20:50:35 +0100

    Update launch.bat

--- launch.bat
+++ launch.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
 echo Starting Splat3D!
-cmd /k C:\Users\JavierJ\AppData\Local\Programs\Python\Python310\python.exe splat.py
+cmd /k C:\Users\\AppData\Local\Programs\Python\Python310\python.exe splat.py
