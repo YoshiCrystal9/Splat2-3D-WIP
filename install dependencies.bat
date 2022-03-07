2e58cb6a3476a66dc6e4deada0508e2468d6b113
YoshiCrystal9 <89108143+YoshiCrystal9@users.noreply.github.com>
2022-03-07 12:28:42 +0100

    Update install dependencies.bat

--- install dependencies.bat
+++ install dependencies.bat
@@ -1,5 +1,7 @@
 @ECHO OFF
 echo Downloading PyOpenGL!
 cmd /k C:/Python34/python.exe C:/Python34/pip install PyOpenGL
+echo Downloading PyQt5!
 cmd /k C:/Python34/python.exe C:/Python34/pip install PyQt5
+echo Downloading byml!
 cmd /k C:/Python34/python.exe C:/Python34/pip install byml
